c5987846a76ff7f1ac3df5634a3d765174c45474
Andrew Monks <a@monks.co>
2015-12-10 22:03:06 -0600

    express

--- app.js
+++ app.js
@@ -6,20 +6,38 @@ var express = require('express')
 var app = express().createServer()
 var io = require('socket.io')(app)
 
+var latest = 0
+
+var next_video = function () {
+  return latest + 1
+}
+
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' })
-  socket.on('my other event', function (data) {
-    console.log(data)
+  // socket.emit('news', { hello: 'world' })
+  // socket.on('my other event', function (data) {
+  //   console.log(data)
+  // })
+
+  var i = next_video()
+  var camera = new RaspiCam(video_opts({name: i}))
+  setInterval(camera.start, process.env.FREQUENCY || 1000)
+
+  camera.on('started', function (err, timestamp) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('video started at ' + timestamp)
+    }
+  })
+
+  camera.on('exit', function (timestamp) {
+    // we can now do stuff with the captured image, which is stored in /data
+    console.log('video child process has exited at ' + timestamp)
+    socket.emit('captured', { url: '/videos/' + i + '.h264' })
   })
 })
 
 app.use('/videos', express.static('/data'))
-
-// app.get('/capture', function (req, res) {
-//   camera.start()
-//   console.log('capturing')
-//   res.send('capturing')
-// })
 
 app.get('/latest', function (req, res) {
   res.redirect('/videos/latest.h264')
@@ -29,7 +47,7 @@ app.get('/list', function (req, res) {
   res.send('hello')
 })
 
-var video_opts = function () {
+var video_opts = function (opts) {
   let flips = {}
   if (process.env.HF === 'TRUE') {
     flips.hf = true
@@ -39,37 +57,16 @@ var video_opts = function () {
   }
   let defaults = {
     mode: 'video',
-    output: '/data/latest.h264',
+    output: '/data/' + opts.name + '.h264',
     width: process.env.VIDEO_WIDTH || 960,
     height: process.env.VIDEO_HEIGHT || 540,
     framerate: process.env.VIDEO_FRAMERATE || 15,
     timeout: process.env.VIDEO_LENGTH || 3000
   }
-  return Object.assign(defaults, flips)
+  Object.assign(opts, flips)
+  Object.assign(opts, defaults)
+  return opts
 }
-
-var camera = new RaspiCam(video_opts())
-
-camera.on('started', function (err, timestamp) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('video started at ' + timestamp)
-  }
-})
-
-camera.on('read', function (err, timestamp, filename) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('video capturing with filename: ' + filename)
-  }
-})
-
-camera.on('exit', function (timestamp) {
-  console.log('video child process has exited at ' + timestamp)
-  // we can now do stuff with the captured image, which is stored in /data
-})
 
 var server = app.listen(3000, function () {
   var host = server.address().address
