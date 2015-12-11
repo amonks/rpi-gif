832579f345b8bcf550a5b0ecc9ebc55b2e3f53dd
Andrew Monks <a@monks.co>
2015-12-10 19:35:16 -0600

    app

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
+// rpi-gif
 
-var RaspiCam = require("raspicam")
-var app = require("express")()
+var RaspiCam = require('raspicam')
+var app = require('express')()
 
 app.get('/capture', function (req, res) {
   res.send('capturing')
   camera.start()
   console.log('capturing')
-}
+})
 
 var camera = new RaspiCam({
-  mode: "video",
-  output: "../data/video.h264",
-  framerate: process.env.VIDEO_FRAMERATE || 15
+  mode: 'video',
+  output: '../data/video.h264',
+  framerate: process.env.VIDEO_FRAMERATE || 15,
   timeout: process.env.VIDEO_LENGTH || 3000
 })
 
-camera.on("started", function( err, timestamp ){
-  console.log("video started at " + timestamp )
+camera.on('started', function (err, timestamp) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('video started at ' + timestamp)
+  }
 })
 
-
-camera.on("read", function( err, timestamp, filename ){
-  console.log("video captured with filename: " + filename )
-  // we can now do stuff with the captured image, which is stored in /data
+camera.on('read', function (err, timestamp, filename) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('video captured with filename: ' + filename)
+    // we can now do stuff with the captured image, which is stored in /data
+  }
 })
 
-camera.on("exit", function( timestamp ){
-  console.log("video child process has exited at " + timestamp )
+camera.on('exit', function (timestamp) {
+  console.log('video child process has exited at ' + timestamp)
 })
-
 
