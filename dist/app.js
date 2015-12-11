611dcd863e6e18c9b57cd7f4f08538a4479f36a0
Andrew Monks <a@monks.co>
2015-12-11 10:16:09 -0600

    no-iojs

--- dist/app.js
+++ dist/app.js
@@ -55,7 +55,7 @@ camera.on('exit', function (timestamp) {
   console.log('video child process has exited at ' + timestamp);
   console.log('now gonna start video conversion process');
 
-  var command = 'avconv -y -r 30 -i /data/vid.264 -vcodec copy /data/vid.mp4 && avconv -y -i /data/vid.mp4 -s ' + (process.env.VIDEO_FRAMERATE || 15) + ' -vf scale=' + (process.env.VIDEO_WIDTH || 480) + ':' + (process.env.VIDEO_HEIGHT || 270) + ',format=rgb8,format=rgb24 -r 10 /data/vid.gif';
+  var command = 'avconv -y -r 30 -i /data/vid.264 -vcodec copy /data/vid.mp4 && avconv -y -i /data/vid.mp4 -vf scale=' + (process.env.VIDEO_WIDTH || 480) + ':' + (process.env.VIDEO_HEIGHT || 270) + ',format=rgb8,format=rgb24 -r 10 /data/vid.gif';
   console.log(command);
   exec(command, function (error, stdout, stderr) {
     if (error) {
